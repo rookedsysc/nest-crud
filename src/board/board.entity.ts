16b98efd73458360dcb806515784c25a0fef5b6f
rookedsysc <rookedsysc36@gmail.com>
2024-09-04 17:08:20 +0900

    feat: Board Entity 개발

---
+++ src/board/board.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Board {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+}
