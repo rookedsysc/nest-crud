a316b75b109ac0c14a7a2a07321e1e51424df337
rookedsysc <rookedsysc36@gmail.com>
2024-09-04 11:30:39 +0900

    feat: Swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Board CRUD')
+    .setDescription('내 첫 Nest')
+    .setVersion('1.0')
+    .addTag('rookedsysc')
+    .build();
+  const documents = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, documents);
+
   await app.listen(3000);
 }
 bootstrap();
