1410948809ba586cce38fabe3a987771d446f978
rookedsysc <rookedsysc36@gmail.com>
2024-09-05 14:48:26 +0900

    feat: Comment Entity 개발

---
+++ src/typeorm/comment/comment.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Board } from '../board/board.entity';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Board, (board) => board.comments, {
+    cascade: true,
+    eager: true,
+  })
+  board: Board;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  constructor(title: string, content: string, board: Board) {
+    this.title = title;
+    this.content = content;
+    this.board = board;
+  }
+}
