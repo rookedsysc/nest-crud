71b56d9597640817c64b1629b6fd69a8ad4d8c78
rookedsysc <rookedsysc36@gmail.com>
2024-09-05 15:37:44 +0900

    update: Board Response에 Comment 포함 Board Update시 반환값 삭제

--- src/typeorm/board/board.service.ts
+++ src/typeorm/board/board.service.ts
@@ -5,6 +5,8 @@ import { Board } from './board.entity';
 import { BoardReq } from './dto/board-req.dto';
 import { BoardResp } from './dto/board-response.dto';
 import { BoardUpdateReq } from './dto/board-update.dto';
+import { CommentResp } from '../comment/dto/comment-resp.dto';
+import { title } from 'process';
 
 @Injectable()
 export class BoardService {
@@ -21,10 +23,7 @@ export class BoardService {
     return await this.boardRepository.save(board);
   }
 
-  async updateBoard(
-    id: number,
-    boardUpdateReq: BoardUpdateReq,
-  ): Promise<Board> {
+  async updateBoard(id: number, boardUpdateReq: BoardUpdateReq) {
     const board = await this.boardRepository.findOne({ where: { id } });
     if (!board) {
       throw new NotFoundException(`Board with ID ${id} not found`);
@@ -36,16 +35,31 @@ export class BoardService {
     if (boardUpdateReq.content) {
       board.content = boardUpdateReq.content;
     }
-
-    return await this.boardRepository.save(board);
+    this.boardRepository.save(board);
   }
 
   async findBoardResp(id: number): Promise<BoardResp> {
     const board = await this.findBoardOrThrow(id);
-    const boardResp = new BoardResp();
-    boardResp.id = board.id;
-    boardResp.title = board.title;
-    boardResp.content = board.content;
+
+    // comment DTO 생성
+    const comments = await board.comments;
+    const commentResponses: CommentResp[] = comments.map((comment) => {
+      const commentResp = new CommentResp(
+        comment.id,
+        comment.title,
+        comment.content,
+        comment.createdAt,
+      );
+      return commentResp;
+    });
+
+    // board DTO
+    const boardResp = new BoardResp(
+      board.id,
+      board.title,
+      board.content,
+      commentResponses,
+    );
 
     return boardResp;
   }
@@ -63,13 +77,25 @@ export class BoardService {
   async getAllBoards(): Promise<BoardResp[]> {
     const boards = await this.boardRepository.find();
 
-    const boardResponses: BoardResp[] = boards.map((board) => {
-      const boardResp = new BoardResp();
-      boardResp.id = board.id;
-      boardResp.title = board.title;
-      boardResp.content = board.content;
-      return boardResp;
+    const futureResponses = boards.map(async (board) => {
+      const comments = await board.comments;
+      const commentResponses: CommentResp[] = comments.map((comment) => {
+        return new CommentResp(
+          comment.id,
+          comment.title,
+          comment.content,
+          comment.createdAt,
+        );
+      });
+
+      return new BoardResp(
+        board.id,
+        board.title,
+        board.content,
+        commentResponses,
+      );
     });
+    const boardResponses = await Promise.all(futureResponses);
 
     return boardResponses;
   }
--- src/typeorm/board/dto/board-response.dto.ts
+++ src/typeorm/board/dto/board-response.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { CommentResp } from 'src/typeorm/comment/dto/comment-resp.dto';
 
 export class BoardResp {
   @ApiProperty({ description: 'Board ID' })
@@ -7,4 +8,18 @@ export class BoardResp {
   title: string;
   @ApiProperty({ description: '본문 내용' })
   content: string;
+  @ApiProperty({ description: '댓글 목록' })
+  comments: CommentResp[];
+
+  constructor(
+    id: number,
+    title: string,
+    content: string,
+    comments: CommentResp[],
+  ) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.comments = comments;
+  }
 }
